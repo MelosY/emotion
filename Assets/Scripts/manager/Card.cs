3d7f2fdd9aa75cd9dd150ff978c1670189fcb9fa
lvskiller <1450848922@qq.com>
2019-03-21 13:08:06 +0800

    m

--- Assets/Scripts/manager/Card.cs
+++ Assets/Scripts/manager/Card.cs
@@ -1,22 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Card : MonoBehaviour {
 
     public class Card_
     {
+          public GameObject image;
           public string name;
           public int[] types;
           public int[] counts;
     }
     
     List<List<Card_>> decks=new List<List<Card_>> ();
-
-    public void createCard(int type,string name, int[] types,int[] counts)
+    List<List<Card_>> handCard=new List<List<Card_>> ();
+    public void createCard(int type,GameObject image,string name, int[] types,int[] counts)
     {
             Card_ temp =new Card_();
             temp.name = name;
+            temp.image = image;
             temp.types = types;
             temp.counts = counts;
             decks[type].Add(temp);
@@ -52,6 +55,11 @@ public class Card : MonoBehaviour {
     public void destroyDeck(int type)
     {
             decks.Remove(decks[type]);
+    }
+
+    public void washDeck(int type)
+    {
+        
     }
 }
 
--- Assets/Scripts/manager/cardController.cs
+++ Assets/Scripts/manager/cardController.cs
@@ -1,27 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class cardController : MonoBehaviour
 {
 	public Card card;
-	
+	public GameObject[] Images;
 
 	public void create0()
 	{
 		int[] temp_type={1};
 		int[] temp_counts={2};
-		card.createCard(0,"attack",temp_type,temp_counts);
-		card.createCard(0,"attack",temp_type,temp_counts);
+		card.createCard(0,Images[0],"attack",temp_type,temp_counts);
+		card.createCard(0,Images[0],"attack",temp_type,temp_counts);
 	}
 
 	public void create1()
 	{
 		int[] temp_type={1};
 		int[] temp_counts={2};
-		card.createCard(1,"attack",temp_type,temp_counts);
-		card.createCard(1,"attack",temp_type,temp_counts);
+		card.createCard(0,Images[0],"attack",temp_type,temp_counts);
+		card.createCard(0,Images[0],"attack",temp_type,temp_counts);
 	}
 
+	public void createCard(GameObject image, Vector3 position,Quaternion createRotation)
+	{
+		GameObject itemGo = Instantiate(image, position, createRotation);
+		itemGo.transform.SetParent(gameObject.transform);
+		itemPositionList.Add(createPosition);
+
+	}
 	
 }

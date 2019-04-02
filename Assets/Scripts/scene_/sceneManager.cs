a7f4ced915de225c54bdcbc88240807dd9eabcce
lvskiller <1450848922@qq.com>
2019-04-02 11:53:58 +0800

    1

---
+++ Assets/Scripts/scene_/sceneManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sceneManager : MonoBehaviour {
+
+	private Vector3 offset=new Vector3(-42f,-9.7f,-10);
+	public Camera camera;
+	void Start()
+	{
+		
+	}
+
+
+	void Update()
+	{
+		if (Input.GetKeyUp("n"))
+		{
+			camera.transform.position = offset;
+		}
+	}
+}
